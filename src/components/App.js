ec407a12e899876786db3dbf92d339c7800d94c9
Adil Naimi <adilnm23@gmail.com>
2020-05-23 05:23:42 -0500

    create a GoogleAuth component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import streamShow from "./streams/StreamShow";
 import StreamList from "./streams/StreamList";
+import Header from "./Header";
+
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <Router>
         <div>
-            <Route exact path="/" component={StreamList} />
-            <Route exact path="/streams/new" component={StreamCreate} />
-            <Route exact path="/streams/edit" component={StreamEdit} />
-            <Route exact path="/streams/delete" component={StreamDelete} />
-            <Route exact path="/streams/show" component={streamShow} />
+          <Header />
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={streamShow} />
         </div>
       </Router>
     </div>
