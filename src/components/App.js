ab3b0e0314f33b339974f7441e8e4ad42512d02b
Adil Naimi <adilnm23@gmail.com>
2020-05-23 02:49:50 -0500

    create crud components

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, link } from 'react-router-dom';
+
+const App = ()=>{
+    return(
+        <div>App</div>
+    )
+}
+
+export default App
