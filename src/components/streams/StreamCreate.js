31b0e6890030239eff8d95f8e4b254c33be02e28
Adil Naimi <adilnm23@gmail.com>
2020-05-26 22:29:41 -0500

    Create a submit button

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,19 +1,31 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
-  renderInput = ({input, label}) => {
+  renderInput = ({ input, label }) => {
     return (
       <div className="field">
-          <label>{label}</label>
-          <input {...input} />
+        <label>{label}</label>
+        <input {...input} />
       </div>
     );
   };
+
+  onSubmit(formValues) {
+
+  }
   render() {
     return (
-      <form className="ui form">
-        <Field name="title" component={this.renderInput} label="Enter Title"/>
-        <Field name="description" component={this.renderInput} label="Enter Description"/>
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
